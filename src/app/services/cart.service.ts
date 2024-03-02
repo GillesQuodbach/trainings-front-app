a15d857d1b24c0465209ae7aeb23bc0e4abfd0fd
GillesQuodbach <quodbach.gilles@gmail.com>
2024-03-02 15:33:26 +0100

    externalisation data trainings

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,41 +2,20 @@ import { CartItem } from 'src/app/model/cartItem';
 import { Training } from 'src/app/model/training';
 import { Customer } from 'src/app/model/customer';
 import { Injectable } from '@angular/core';
+import { listTrainings } from 'src/assets/data/listTrainings';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  constructor() {
-    this.retrieveData;
-  }
-
-  listTrainings = [
-    {
-      id: 1,
-      name: 'Java',
-      description: 'Formation Java SE 8 sur 5 jours',
-      price: 1500,
-      quantity: 1,
-    },
-    {
-      id: 2,
-      name: 'DotNet',
-      description: 'Formation DotNet 3 jours',
-      price: 1000,
-      quantity: 1,
-    },
-    {
-      id: 3,
-      name: 'Python',
-      description: 'Formation Python/Django sur 5 jours',
-      price: 1500,
-      quantity: 1,
-    },
-  ];
-
+  listTrainings: Training[] = [];
   listCart: CartItem[] = [];
 
+  constructor() {
+    this.retrieveData;
+    this.listTrainings = listTrainings
+  }
   customer: Customer = {
     name: '',
     firstName: '',
